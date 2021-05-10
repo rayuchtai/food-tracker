f3b4124a40a1afeb54866d4ec1849aa8c2e56c12
Rachel Taima <racheltaima@Rachels-MacBook-Air.local>
2021-05-10 11:59:57 -0700

    npm i dotenv and mongoose

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const mongoose = require('mongoose')
 
 const app = express();
+require('dotenv').config()
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
 
-const PORT = 3000;
+//middleware:
+app.use(express.json())
+app.use(express.static('public'))
 
 app.listen(PORT, () => {
   console.log("🍙 Listening on port " + PORT);
@@ -11,3 +17,21 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+
+// Error / success
+mongoose.connection.on('error', err =>
+  console.log(
+    err.message,
+    ' is Mongod not running?/Problem with Atlas Connection?'
+  )
+)
+mongoose.connection.on('connected', () =>
+  console.log('mongo connected: ', MONGODB_URI)
+)
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
